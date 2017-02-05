fc2324ce37cb59f586ef7a5eb510cc4d1ea8a666
AnkurBeginning <201551016.iiitvadodara.ac.in@gmail.com>
2017-02-05 23:10:26 +0530

    feat: addded project

---
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -0,0 +1,30 @@
+package com.example.android.miwok;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class NumbersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_numbers);
+
+        String[] words=new String[10];
+
+        words[0]="one";
+        words[1]="two";
+        words[2]="three";
+        words[3]="four";
+        words[4]="five";
+        words[5]="six";
+        words[6]="seven";
+        words[7]="eight";
+        words[8]="nine";
+        words[9]="ten";
+
+        Log.v("NumbersActivity","Words at index 0 "+ words[0]);
+        Log.v("NumbersActivity","Words at index 1 "+ words[1]);
+    }
+}
