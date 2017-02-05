74cc97563b5884fdb5e913bc21dcfe862265b139
AnkurBeginning <201551016.iiitvadodara.ac.in@gmail.com>
2017-02-06 00:31:37 +0530

    feat: Array changed to ArrayList

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.util.ArrayList;
+
 public class NumbersActivity extends AppCompatActivity {
 
     @Override
@@ -11,20 +13,22 @@ public class NumbersActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
 
-        String[] words=new String[10];
+        ArrayList<String> words=new ArrayList<String>();
 
-        words[0]="one";
-        words[1]="two";
-        words[2]="three";
-        words[3]="four";
-        words[4]="five";
-        words[5]="six";
-        words[6]="seven";
-        words[7]="eight";
-        words[8]="nine";
-        words[9]="ten";
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
 
-        Log.v("NumbersActivity","Words at index 0 "+ words[0]);
-       
+
+        for(int i=0;i<10;i++)
+        Log.v("NumbersActivity","Words at index " + (i+1) + " "+ words.get(i));
+
     }
 }
